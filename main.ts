9448fab103c7af891063e5698d9c8faaf0e09596
faheemibrahim321 <161450679+faheemibrahim321@users.noreply.github.com>
2024-02-27 04:38:07 -0800

    third assigment 3 class
    third assigment 3 class

---
+++ main.ts
@@ -0,0 +1,7 @@
+let name1 ="helloword"
+console.log(name1)
+name1="faheem ibrahim"
+console.log(name1)
+let user_name = "faheem ibrahim"
+let sayName = "faheem ibrahim"
+//let@name = "faheem ibrahim"
